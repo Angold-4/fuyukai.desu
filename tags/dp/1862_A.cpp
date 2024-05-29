525734a1732afcfc68be62887fac07c3275c8547
Angold-4 <awang@angold4.org>
2024-05-29 20:47:34 +0800

    w2d2 1 question, palindrome next

---
+++ tags/dp/1862_A.cpp
@@ -0,0 +1,108 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <sstream>
+#include <set>
+#include <vector>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+// https://codeforces.com/blog/entry/76087
+template<class T1, class T2>
+std::ostream &operator<<(std::ostream &os, const std::pair<T1,T2> &p){
+  os << '{' << p.first << ", " << p.second << '}';
+  return os;
+}
+
+template<typename C,
+         typename T = std::decay<decltype(*begin(std::declval<C>()))>,
+         typename std::enable_if<!std::is_same<C, std::string>::value>::type* = nullptr
+         >
+std::ostream &operator<<(std::ostream &os, const C &container){
+  bool first = true;
+  std::stringstream ss;
+  ss << '[';
+  for(const auto &x : container){
+    if (!first){
+      ss << ", ";
+    }
+    first = false;
+    ss << x;
+  }
+  ss << ']';
+  return os << ss.str();
+}
+
+vector<string> vec_splitter(string s) {
+	s += ',';
+	vector<string> res;
+	while(!s.empty()) {
+		res.push_back(s.substr(0, s.find(',')));
+		s = s.substr(s.find(',') + 1);
+	}
+	return res;
+}
+
+void debug_out(
+vector<string> __attribute__ ((unused)) args,
+__attribute__ ((unused)) int idx,
+__attribute__ ((unused)) int LINE_NUM) { cerr << endl; }
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+	if(idx > 0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+	stringstream ss; ss << H;
+	cerr << args[idx] << " = " << ss.str();
+	debug_out(args, idx + 1, LINE_NUM, T...);
+}
+
+#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+#define ll long long
+
+void run_case() {
+  // Will TLE if do it row by row
+  int n, m; cin >> n >> m;
+  vector<string> carpet(n);
+  for (int i = 0; i < n; i++) cin >> carpet[i];
+
+  string vika = "vika";
+  int fd = 0; // dp-like problem, sub-problems
+
+  for (int i = 0; i < m; i++) {
+    bool check = false;
+    // do it column by column
+    for (int j = 0; j < n; j++) {
+      if (carpet[j][i] == vika[fd]) {
+	check = true;
+	break;
+      }
+    }
+    if (check) {
+      fd++;
+      if (fd == 4) break;
+    }
+  }
+
+  if (fd == 4) cout << "YES" << '\n';
+  else cout << "NO" << '\n';
+}
+
+int main() {
+  int tests;
+  cin >> tests;
+
+  while (tests-- > 0) 
+    run_case();
+}
